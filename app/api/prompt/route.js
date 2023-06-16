560df9fce889b09f34fca7ad830a6283b4a18318
Osman Sultan <o.sultan2811@gmail.com>
2023-06-15 21:19:39 -0400

    another attempt

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,14 +1,23 @@
 import Prompt from "@models/prompt";
-import { connectToDb } from "@utils/database";
+import { connectToDB } from "@utils/database";
 
 export const GET = async (request) => {
   try {
-    await connectToDb();
-
+    await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
+    const response = new Response(JSON.stringify(prompts), { status: 200 });
+    const url = new URL(request.url);
+    url.searchParams.set("t", Date.now());
+    response.headers.set(
+      "Cache-Control",
+      "no-cache, no-store, must-revalidate"
+    );
+    response.headers.set("Pragma", "no-cache");
+    response.headers.set("Expires", "0");
+    response.headers.set("Location", url.toString());
 
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return response;
   } catch (error) {
-    return new Response("Failed to fetch all prompts", { status: 500 });
+    return new Response("failed to fetch all prompts", { status: 500 });
   }
 };
